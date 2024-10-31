b03f7d7f4f95596cd724a2af9c516a2e355fbe34
rtretyakov <rtretyakov@outlook.com>
2024-10-31 14:24:28 +1000

    Перенес в RouteModel методы для получения данных для заголовка

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,8 +1,8 @@
-import { getDestinationById } from '../mock/destination.js';
-import { getFormattedRangeDate } from '../util.js';
-import { getOfferById } from '../mock/offer.js';
+// import { getDestinationById } from '../mock/destination.js';
+// import { getFormattedRangeDate } from '../util.js';
+// import { getOfferById } from '../mock/offer.js';
 
-import dayjs from 'dayjs';
+// import dayjs from 'dayjs';
 
 
 const getEventLength = (dateFrom, dateTo) => {
@@ -29,39 +29,39 @@ export const DEFAULT_SORT_METHOD = sortMethods[DEFAULT_SORT_TYPE];
 export const capitalize = (string) => string.charAt(0).toUpperCase() + string.slice(1);
 
 
-export const getTripTitle = (route) => {
-  const pointsList = new Array();
-  route.forEach((item) => pointsList.push(getDestinationById(item.destination).name));
+// export const getTripTitle = (route) => {
+//   const pointsList = new Array();
+//   route.forEach((item) => pointsList.push(getDestinationById(item.destination).name));
 
-  if (pointsList.length > 3) {
-    return `${[pointsList[0]]} - ... - ${[pointsList[pointsList.length - 1]]}`;
-  }
+//   if (pointsList.length > 3) {
+//     return `${[pointsList[0]]} - ... - ${[pointsList[pointsList.length - 1]]}`;
+//   }
 
-  return pointsList.join(' - ');
-};
-
-
-export const getTripDates = (route) => {
-  const copyRoute = route.slice().sort(sortByDate);
-  const startDate = dayjs(copyRoute[0].date_from);
-  const endDate = dayjs(copyRoute.slice(-1)[0].date_to);
-
-  return `${getFormattedRangeDate(startDate.date(), startDate.month() + 1, endDate.date(), endDate.month() + 1)}`;
-};
+//   return pointsList.join(' - ');
+// };
 
 
-export const getTripCost = (route) => {
-  // Здесь подсчет стоимости маршрута. Суммируется стоимость каждой точки маршрута плюс стоимость offers  для точки маршрута
-  let cost = 0;
-  for (let i = 0; i < route.length; i++) {
-    cost += route[i].base_price;
+// export const getTripDates = (route) => {
+//   const copyRoute = route.slice().sort(sortByDate);
+//   const startDate = dayjs(copyRoute[0].date_from);
+//   const endDate = dayjs(copyRoute.slice(-1)[0].date_to);
 
-    if (route[i].offers && route[i].offers.length) {
-      for (let j = 0; j < route[i].offers.length; j++) {
-        cost += getOfferById(route[i].offers[j]).price;
-      }
-    }
-  }
+//   return `${getFormattedRangeDate(startDate.date(), startDate.month() + 1, endDate.date(), endDate.month() + 1)}`;
+// };
 
-  return cost;
-};
+
+// export const getTripCost = (route) => {
+//   // Здесь подсчет стоимости маршрута. Суммируется стоимость каждой точки маршрута плюс стоимость offers  для точки маршрута
+//   let cost = 0;
+//   for (let i = 0; i < route.length; i++) {
+//     cost += route[i].base_price;
+
+//     if (route[i].offers && route[i].offers.length) {
+//       for (let j = 0; j < route[i].offers.length; j++) {
+//         cost += getOfferById(route[i].offers[j]).price;
+//       }
+//     }
+//   }
+
+//   return cost;
+// };
